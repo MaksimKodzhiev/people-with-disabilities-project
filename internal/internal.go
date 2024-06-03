97ffa2d5c3b8197f1828cb05c3042a1cf291525a
shakal <temporarybond@yandex.ru>
2024-06-03 21:30:52 +0300

    Added internal.go file

--- internal/internal.go
+++ internal/internal.go
@@ -1,1 +1,61 @@
 package internal
+
+import (
+    "context"
+    "errors"
+    "fmt"
+    "log"
+    "net/http"
+    "os"
+    "os/signal"
+    "syscall"
+
+    . "project/internal/api/constants"
+    // "project/internal/api/middleware"
+)
+
+func RunHTTPServer() (err, shutdownErr error) {
+    // middlewareChain := middlewarepkg.Chain(
+    //     middlewarepkg.WrapResponseWriter,
+    //     middlewarepkg.Logger,
+    //     middlewarepkg.Timeout,
+    //     middlewarepkg.Recoverer,
+    //     middlewarepkg.RedirectSlashes,
+    // )
+
+    server := http.Server{
+        Addr:         ":http",
+        ReadTimeout:  ReadTimeout,
+        WriteTimeout: WriteTimeout + RequestTimeout,
+        IdleTimeout:  IdleTimeout,
+        // Handler:      middlewareChain(handlers.Root),
+    }
+
+    go func() {
+        log.Println("Server is listening on", server.Addr)
+
+        err = server.ListenAndServe()
+
+        if !errors.Is(err, http.ErrServerClosed) {
+            err = fmt.Errorf("RunHTTPServer: http.(*Server).ListenAndServe: %w", err)
+        } else {
+            err = nil
+        }
+    }()
+
+    signalChannel := make(chan os.Signal, 1)
+    signal.Notify(signalChannel, syscall.SIGINT, syscall.SIGQUIT, syscall.SIGTERM)
+    <-signalChannel
+
+    ctx, cancel := context.WithTimeout(context.Background(), WriteTimeout+RequestTimeout)
+
+    defer cancel()
+
+    shutdownErr = server.Shutdown(ctx)
+
+    if shutdownErr != nil {
+        shutdownErr = fmt.Errorf("RunHTTPServer: http.(*Server).Shutdown: %w", shutdownErr)
+    }
+
+    return err, shutdownErr
+}
