b5a9cdb5ba69f6dc423aab43fc220a185bed761f
shakal <temporarybond@yandex.ru>
2024-06-03 21:36:59 +0300

    Implemented main function

--- cmd/backend/main.go
+++ cmd/backend/main.go
@@ -1,5 +1,23 @@
-package backend
+package main
+
+import (
+    "log"
+
+    project "project/internal"
+)
 
 func main() {
-    //
+    err, shutdownErr := project.RunHTTPServer()
+
+    if err == nil && shutdownErr == nil {
+        log.Println("Server terminated successfully!")
+    } else {
+        if shutdownErr != nil {
+            log.Println(shutdownErr)
+        }
+
+        if err != nil {
+            log.Println(err)
+        }
+    }
 }
